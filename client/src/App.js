255b2bcbd8ec403c18c9eb29479eefe39cf6cb6a
Alaa Jerbi <mralaajerbi@gmail.com>
2020-10-11 14:39:35 +0100

    Update CSS

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,15 @@ import CreateChatRoomView from './components/chat/CreateChatRoomView';
 import ChatView from './components/chat/ChatView';
 
 class App extends Component {
+  componentWillMount() {
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }
+
   render() {
     return (
       <Router>
