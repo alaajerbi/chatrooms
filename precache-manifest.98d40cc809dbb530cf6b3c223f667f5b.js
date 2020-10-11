3497fbbeef973a3f198e7befcd0d6f266505facb
Alaa Jerbi <mralaajerbi@gmail.com>
2020-10-11 10:02:12 +0100

    Updates

---
+++ precache-manifest.98d40cc809dbb530cf6b3c223f667f5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c950c3e52fbd596a00d9",
+    "url": "/chatrooms/static/css/main.6c3d1e05.chunk.css"
+  },
+  {
+    "revision": "c950c3e52fbd596a00d9",
+    "url": "/chatrooms/static/js/main.c950c3e5.chunk.js"
+  },
+  {
+    "revision": "151d661e01b4b449ab95",
+    "url": "/chatrooms/static/js/runtime~main.151d661e.js"
+  },
+  {
+    "revision": "89b8ae2893687fa57ee0",
+    "url": "/chatrooms/static/js/2.89b8ae28.chunk.js"
+  },
+  {
+    "revision": "1caff7fb79b2512ee29e1e3cd61ccd88",
+    "url": "/chatrooms/static/media/logo.1caff7fb.svg"
+  },
+  {
+    "revision": "632c2b42918e63e0deee513d2f05db41",
+    "url": "/chatrooms/static/media/send.632c2b42.svg"
+  },
+  {
+    "revision": "a6b4932baafd0f1cd4bebe241ef73a9c",
+    "url": "/chatrooms/static/media/notification.a6b4932b.ogg"
+  },
+  {
+    "revision": "5840e887da87a583d89b91d935f73e7a",
+    "url": "/chatrooms/index.html"
+  }
+];
