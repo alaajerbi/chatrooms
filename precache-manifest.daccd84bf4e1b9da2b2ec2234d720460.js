eaadc05e388a39af6830a632fa34e601785a5106
Alaa Jerbi <mralaajerbi@gmail.com>
2020-11-25 09:49:13 +0100

    Updates

---
+++ precache-manifest.daccd84bf4e1b9da2b2ec2234d720460.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3d473a5eb14931af361",
+    "url": "/chatrooms/static/css/main.d8e6c704.chunk.css"
+  },
+  {
+    "revision": "c3d473a5eb14931af361",
+    "url": "/chatrooms/static/js/main.c3d473a5.chunk.js"
+  },
+  {
+    "revision": "151d661e01b4b449ab95",
+    "url": "/chatrooms/static/js/runtime~main.151d661e.js"
+  },
+  {
+    "revision": "89b8ae2893687fa57ee0",
+    "url": "/chatrooms/static/js/2.89b8ae28.chunk.js"
+  },
+  {
+    "revision": "1caff7fb79b2512ee29e1e3cd61ccd88",
+    "url": "/chatrooms/static/media/logo.1caff7fb.svg"
+  },
+  {
+    "revision": "632c2b42918e63e0deee513d2f05db41",
+    "url": "/chatrooms/static/media/send.632c2b42.svg"
+  },
+  {
+    "revision": "a6b4932baafd0f1cd4bebe241ef73a9c",
+    "url": "/chatrooms/static/media/notification.a6b4932b.ogg"
+  },
+  {
+    "revision": "c24288ff12ef53a32c969f256c7d8932",
+    "url": "/chatrooms/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5803752e54867cada5cad7e41309151.js"
+  "/chatrooms/precache-manifest.daccd84bf4e1b9da2b2ec2234d720460.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/chatrooms/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
