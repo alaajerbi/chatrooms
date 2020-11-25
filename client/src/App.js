b213bb273537c6ac1ecad1cf2c2cdd11ede732dd
Alaa Jerbi <mralaajerbi@gmail.com>
2020-11-25 11:10:12 +0100

    Use HashRouter instead of BrowserRouter for Gh pages redirection

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import LandingPage from './components/LandingPage.js';
 import CreateChatRoomView from './components/chat/CreateChatRoomView';
