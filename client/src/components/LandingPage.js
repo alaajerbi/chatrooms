9d02b00e38f173e3bb3fd665a01ae8b44811dfac
Alaa Jerbi <mralaajerbi@gmail.com>
2020-11-25 09:38:47 +0100

    Update landing page

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import logo from '../assets/svg/logo.svg';
 import '../App.css';
 
-const LandingPage = (props) => {
+const LandingPage = () => {
   return (
     <div className="landing-page">
 
@@ -12,7 +12,7 @@ const LandingPage = (props) => {
           <img className="logo" src={logo} alt="Logo" width="100%" />
 
         <h1>Chat Rooms with Time Limit</h1>
-        <p>Built with ReactJS, NodeJS, Express, and Socket.io by <a href="http://alaajerbi.com">@alaajerbi</a></p>
+        <p>Built with ReactJS, NodeJS, Express, and Socket.io by <a href="http://alaajerbi.com">Alaa Jerbi</a></p>
         <div className="cta">
           <Link to='createChatRoom' className="btn" style={{ marginLeft: '10px' }}>Create a room!</Link>
         </div>
